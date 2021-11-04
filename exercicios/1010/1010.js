5a4b5f7d94f9de8bf24579f96674076edde42dda
Luiz Felipe Pinho <47011421+LuizFelipePinho@users.noreply.github.com>
2021-11-04 14:01:34 -0300

    Update 1010.js

--- exercicios/1010/1010.js
+++ exercicios/1010/1010.js
@@ -1,15 +1,12 @@
 var input = require('fs').readFileSync('../../dev/stdin/file.txt', 'utf-8');
 var lines = input.split('\n');
 
-var primeiro = lines.shift().split(" ");
-var cod1 = parseInt(primeiro[0]);
-let qtd1 = parseInt(primeiro[1]);
-let valor1 = parseFloat(primeiro[2]);
+var linesOneInp = lines[0].split(' ');
+var linesTwoInp = lines[1].split(' ');
 
-var segundo = lines.shift().split(" ");
-var cod2 = parseInt(segundo[0]);
-let qtd2 = parseInt(segundo[1]);
-let valor2 = parseFloat(segundo[2]);
+const [cod1, qtd1, valor1] = linesOneInp;
+const [cod2, qtd2, valor2] = linesTwoInp;
+
+console.log(`VALOR A PAGAR: R$ ${ ( (qtd1 * valor1) + (qtd2 * valor2) ).toFixed(2) }`)
 
 
-console.log(`VALOR A PAGAR: ${ ( (qtd1 * valor1) + (qtd2 * valor2) ).toFixed(2) }`)
