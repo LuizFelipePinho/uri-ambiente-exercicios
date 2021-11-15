c027bcee2d83c22e61d81c39792760fc637a541c
LuizFelipePinho <luizfelipesilva458@gmail.com>
2021-11-15 18:17:32 -0300

    arrumando os arquivos 1017 e 1018

---
+++ exercicios/1018.js
@@ -0,0 +1,12 @@
+var input = require('fs').readFileSync('../../dev/stdin/file.txt', 'utf-8');
+var lines = input.split('\n');
+
+let num = parseInt(lines.shift());
+
+let notasCem = parseInt( num / 100)
+// let notasCinq = parseInt(( num - (notasCem * 100)) / 50);
+// let notasVint =  
+// console.log(notasVint);
+
+let notasCem2 = Math.trunc(num / 100)
+console.log(notasCem2);
