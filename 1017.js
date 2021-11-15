6e5c30ef4a570adf928e78f02cb0084a93ac4129
LuizFelipePinho <luizfelipesilva458@gmail.com>
2021-11-15 18:24:06 -0300

    alterando localizaçao dos arquvios

--- 1017.js
+++
@@ -1,8 +0,0 @@
-var input = require('fs').readFileSync('../../dev/stdin/file.txt', 'utf-8');
-var lines = input.split('\n');
-
-let tem = parseInt(lines.shift());
-let vel = parseInt(lines.shift());
-
-console.log(`${((vel * tem) / 12).toFixed(3)}`);
-
